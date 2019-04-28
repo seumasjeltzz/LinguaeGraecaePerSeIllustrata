4086826088bf0f93ce994a31e1ea00f4cb95aa51
James Tauber <jtauber@jtauber.com>
2019-04-28 07:14:43 -0400

    move generated PDF to docs folder

--- scripts/pdf.sh
+++ scripts/pdf.sh
@@ -1,5 +1,6 @@
-pandoc -s --toc --template=templates/default.latex -o lgpsi.tex title.txt $(ls -1 src/*.md)
+pandoc -s --toc --template=templates/default.latex -o lgpsi.tex src/title.txt $(ls -1 src/*.md)
 xelatex lgpsi.tex
 # Twice because TOC
 xelatex lgpsi.tex
+mv lgpsi.pdf docs/
 rm lgpsi.{log,aux,toc,tex}
